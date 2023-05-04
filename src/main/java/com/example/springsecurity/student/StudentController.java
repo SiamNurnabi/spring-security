67173568da2c1554b74b144d204f344f1fce1a50
SiamNurnabi <siamnurnabi@gmail.com>
2023-05-04 07:01:02 +0600

    Building an api using student data

---
+++ src/main/java/com/example/springsecurity/student/StudentController.java
@@ -0,0 +1,27 @@
+package com.example.springsecurity.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/students")
+public class StudentController {
+    private static final List<Student> STUDENTS = Arrays.asList(
+            new Student(1, "Sifat"),
+            new Student(2, "Shejuti"),
+            new Student(3, "Mahin")
+    );
+
+    @GetMapping(path = "{studentId}")
+    public Student getStudent(@PathVariable("studentId") Integer studentId) {
+        return STUDENTS.stream()
+                .filter(student -> studentId.equals(student.getStudentId()))
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("Student " + studentId + " does not exist"));
+    }
+}
