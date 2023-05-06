c9bb0d56e20895e7e9737b017960c670c0fa4d4c
Siam-Nurnabi <siamnurnabi@gmail.com>
2023-05-06 12:25:28 +0600

    StudentManagementController class is created

---
+++ src/main/java/com/example/springsecurity/student/StudentManagementController.java
@@ -0,0 +1,40 @@
+package com.example.springsecurity.student;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("management/api/v1/students")
+public class StudentManagementController {
+    private static final List<Student> STUDENTS = Arrays.asList(
+            new Student(1, "Sifat"),
+            new Student(2, "Shejuti"),
+            new Student(3, "Mahin")
+    );
+
+    @GetMapping
+    public List<Student> getAllStudents() {
+        System.out.println("getAllStudents");
+        return STUDENTS;
+    }
+
+    @PostMapping
+    public void registerNewStudent(@RequestBody Student student) {
+        System.out.println("registerNewStudent");
+        System.out.println(student);
+    }
+
+    @DeleteMapping(path = "{studentId}")
+    public void deleteStudent(@PathVariable("studentId") Integer studentId) {
+        System.out.println("deleteStudent");
+        System.out.println(studentId);
+    }
+
+    @PutMapping(path = "{studentId}")
+    public void updateStudent(@PathVariable("studentId") Integer studentId, @RequestBody Student student) {
+        System.out.println("updateStudent");
+        System.out.println(String.format("%s %s", studentId, student));
+    }
+}
